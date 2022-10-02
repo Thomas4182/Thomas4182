9ce1eb84a31a086e518fa4a8690cf0e031b4e42c
Thomas4182 <109043811+Thomas4182@users.noreply.github.com>
2022-10-02 23:24:53 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// These script will remember of change color. 
-document.getElementById("change-theme-btn").addEventListener("click", function() {
-    let darkThemeEnabled = document.body.classList.toggle('dark-theme');
-    localStorage.setItem("dark-theme-enabled", darkThemeEnabled);
-});
-
-if (JSON.parse(localStorage.getItem("dark-theme-enabled"))) {
-    document.body.classList.add("dark-theme");
-}
