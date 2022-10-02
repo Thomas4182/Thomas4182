b00d29ec658f0d56ba7064f3b821d29e2fe24439
Thomas4182 <109043811+Thomas4182@users.noreply.github.com>
2022-10-02 23:24:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// These script will remember of change color. 
+document.getElementById("change-theme-btn").addEventListener("click", function() {
+    let darkThemeEnabled = document.body.classList.toggle('dark-theme');
+    localStorage.setItem("dark-theme-enabled", darkThemeEnabled);
+});
+
+if (JSON.parse(localStorage.getItem("dark-theme-enabled"))) {
+    document.body.classList.add("dark-theme");
+}
